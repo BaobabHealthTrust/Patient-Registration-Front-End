86e1316557b8bb431e6e51497ebb6bf29189b70f
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-26 09:09:33 +0200

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'main#index'
+
   get 'script/index' => 'script#index'
   get 'landing/index' => 'landing#index'
-  root 'users#index'
-  # resources :people
-  # resources :Patients
+
+  resources :people
+  resources :patients
   resources :users
+
 end
