3f47cd198846dc9ac790577f35b870ad6ae253bd
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-27 21:22:54 +0200

    Updated UI look

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'people/people'
+  get "people/people"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'main#index'
+  root "main#index"
 
-  get 'script/index' => 'script#index'
-  get 'landing/index' => 'landing#index'
+  get "/login" => "users#login"
 
   resources :people
   resources :patients
   resources :users
-
 end
