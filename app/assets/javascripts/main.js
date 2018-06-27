870a0e821302c81734a4e92ef03bc661edd0867c
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-27 16:20:10 +0200

    Added styling for the views

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,13 @@
+$(() => {
+    // $('a').click(function (event) {
+    //     // event.preventDefault();
+
+    //     // var href = $(this).attr('href');
+    //     // console.log(href);
+
+    //     // $.get(href, function (page) {
+    //     //     console.log(page);
+    //     //     console.log($(page).find('#main'));
+    //     // });
+    // });
+})
