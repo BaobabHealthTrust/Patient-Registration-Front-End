91d9c27f2eff55a734a7773f71914aba6324a567
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-28 15:16:49 +0200

    Refactored api wrappers in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,43 +1,35 @@
+require 'prs_api/api_client'
+
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
 
-
-    def api_get
-        response, status = ApiClient.new(cookies['x-api-key']).get('/patients')
-            
-        if status == 403
-            redirect controller: :users, action: :login
-            return nil
-        elsif status >= 300
-            if response.nil?
-                logger.error('Failed to communicate with API')
-                flash[:error] = 'Something awful occurred'
-            else
-                flash[:error] = response['errors'].join '\n'
-            return nil
-        else
-          return response
-        end
+  protected
+    def api_get(resource)
+      response, status = ApiClient.new(cookies['x-api-key']).get(resource)
+      handle_api_response response, status
     end
 
-    def api_post(data)
-        response, status = ApiClient.new(cookies['x-api-key']).post('/patients', 
-         data   
-        )
-            
-        if status == 403
-            redirect controller: :users, action: :login
-            return nil
-        elsif status >= 300
-            if response.nil?
-                logger.error('Failed to communicate with API')
-                flash[:error] = 'Something awful occurred'
-            else
-                flash[:error] = response['errors'].join '\n'
-            return nil
-        else
-          return response
-        end
+    def api_post(resource, data)
+      response, status = ApiClient.new(cookies['x-api-key']).post(resource, data)
+      handle_api_response resource, data
     end
 
+  private
+    def handle_api_response(response, status)
+      if status == 403
+        redirect controller: :users, action: :login
+        return nil
+      elsif status >= 300
+        if response.nil?
+          logger.error('Failed to communicate with API')
+          flash[:error] = 'Something awful occurred'
+        else
+          flash[:error] = response['errors'].join '\n'
+        return nil
+      else
+        # Have a 2XX status meaning success however make sure
+        # that response returned is never nil (this flags an error here)
+        return response or {}
+      end
+    end
 end
