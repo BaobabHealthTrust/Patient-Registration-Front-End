61f87ccd47e8b08aa13fab175acc9c513cbcecde
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-29 14:40:28 +0200

    Fixed crash when logged out user navigates to home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "main#index"
+  root "main#check_login"
 
   get "/login" => "main#check_login"
   post "/login" => "main#login"
