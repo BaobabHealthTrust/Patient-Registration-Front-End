7f60996e0d279a881ac889b3867e5de498c195fb
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-29 03:42:29 +0200

    Connected users controller to API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,168 +1,24 @@
+require 'prs_api/api_client'
+
 class UsersController < ApplicationController
   def index
-    # TODO: Retrieve list of users from API
-    @users = SAMPLE_USERS
+    @users = api_get 'users'
   end
 
   def show
-    @user = SAMPLE_USERS[params[:id].to_i]
-    # TODO: Route edit request to API
+    @user = api_get "users/#{params[:id]}"
   end
 
-
   def edit
-    @user = SAMPLE_USERS[params[:id].to_i]
-    # TODO: Route edit request to API
+    @user = api_get "users/#{params[:id]}"
   end
 
   def update
+    @user = api_post "users/#{params[:id]}", params
+    render :show unless @user.nil?
   end
 
   def destroy
     # TODO: Send delete request to API
   end
-
-  def login
-    render layout: "form"
-  end
-
-  private
-
-  SAMPLE_USERS = [
-    {
-      "id": 0,
-      "username": "foobar",
-      "active": true,
-      "person_id": 0,
-      "deleted_at": nil,
-      "created_at": "2018-06-26T07:43:27.000Z",
-      "updated_at": "2018-06-26T07:43:27.000Z",
-      "uuid": nil,
-      "role_id": 1,
-      "role": {
-        "id": 1,
-        "rolename": "manager",
-        "deleted_at": nil,
-        "created_at": "2018-06-26T07:43:27.000Z",
-        "updated_at": "2018-06-26T07:43:27.000Z",
-        "privileges": [],
-      },
-      "person": {
-        "id": 0,
-        "birthdate": "2000-01-01",
-        "gender": "male",
-        "deleted_at": nil,
-        "created_at": "2018-06-26T07:43:27.000Z",
-        "updated_at": "2018-06-26T07:43:27.000Z",
-        "person_name": {
-          "firstname": "foobar",
-          "lastname": "random",
-        },
-        "personal_attributes": [
-          {
-            "id": 0,
-            "value": "121321414132",
-            "person_id": 0,
-            "personal_attribute_type_id": 0,
-            "deleted_at": nil,
-            "created_at": "2018-06-26T07:43:27.000Z",
-            "updated_at": "2018-06-26T07:43:27.000Z",
-            "personal_attribute_type": {
-              "id": 0,
-              "name": "address",
-              "created_at": "2018-06-26T07:43:27.000Z",
-              "updated_at": "2018-06-26T07:43:27.000Z",
-              "deleted_at": nil,
-            },
-          },
-        ],
-      },
-    },
-    {
-      "id": 1,
-      "username": "barfoo",
-      "active": true,
-      "person_id": 1,
-      "deleted_at": nil,
-      "created_at": "2018-06-26T07:43:27.000Z",
-      "updated_at": "2018-06-26T07:43:27.000Z",
-      "uuid": nil,
-      "role_id": 0,
-      "role": {
-        "id": 0,
-        "rolename": "clerk",
-        "deleted_at": nil,
-        "created_at": "2018-06-26T07:43:27.000Z",
-        "updated_at": "2018-06-26T07:43:27.000Z",
-        "privileges": [],
-      },
-      "person": {
-        "id": 1,
-        "birthdate": "2000-01-01",
-        "gender": "male",
-        "deleted_at": nil,
-        "created_at": "2018-06-26T07:43:27.000Z",
-        "updated_at": "2018-06-26T07:43:27.000Z",
-        "person_name": {
-          "firstname": "random",
-          "lastname": "foobar",
-        },
-        "personal_attributes": [
-          {
-            "id": 1,
-            "value": "foobar@foobar.com",
-            "person_id": 1,
-            "personal_attribute_type_id": 1,
-            "deleted_at": nil,
-            "created_at": "2018-06-26T07:43:27.000Z",
-            "updated_at": "2018-06-26T07:43:27.000Z",
-            "personal_attribute_type": {
-              "id": 1,
-              "name": "email",
-              "created_at": "2018-06-26T07:43:27.000Z",
-              "updated_at": "2018-06-26T07:43:27.000Z",
-              "deleted_at": nil,
-            },
-          },
-        ],
-      },
-    },
-    {
-      "id": 2,
-      "username": "jrhacker",
-      "active": true,
-      "person_id": 3,
-      "deleted_at": nil,
-      "created_at": "2018-06-26T07:43:27.000Z",
-      "updated_at": "2018-06-26T07:43:27.000Z",
-      "uuid": nil,
-      "role_id": 0,
-      "role": {
-        "id": 0,
-        "rolename": "clerk",
-        "deleted_at": nil,
-        "created_at": "2018-06-26T07:43:27.000Z",
-        "updated_at": "2018-06-26T07:43:27.000Z",
-        "privileges": [],
-      },
-      "person": {
-        "id": 3,
-        "birthdate": "1990-01-01",
-        "gender": "male",
-        "deleted_at": nil,
-        "created_at": "2018-06-26T07:43:27.000Z",
-        "updated_at": "2018-06-26T07:43:27.000Z",
-        "person_name": {
-          "id": 3,
-          "firstname": "Random",
-          "lastname": "Geek",
-          "person_id": 3,
-          "deleted_at": nil,
-          "created_at": "2018-06-26T07:43:27.000Z",
-          "updated_at": "2018-06-26T07:43:27.000Z",
-        },
-        "personal_attributes": [],
-      },
-    },
-  ]
 end
