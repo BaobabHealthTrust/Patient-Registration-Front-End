6e7bcb3968027e7e6b2fb2371da1ea6d6cf8db78
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-29 13:25:31 +0200

    Added 'new' action to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
-require 'prs_api/api_client'
-
 class UsersController < ApplicationController
   def index
     @users = api_get 'users'
   end
 
+  def new
+    @firstname = params[:firstname] or ''
+    @lastname = params[:lastname] or ''
+
+    render 'layout': 'form'
+  end
+
+  def create
+    person = api_post '/people', params
+
+    if person.nil?
+      flash[:error] = 'Failed to create user (could not create related person)'
+      return
+    end
+    
+    user = api_post '/users', {person_id: person['id']}
+    redirect_to users_path(id: params[:id]) unless user.nil?
+  end
+  
   def show
     @user = api_get "users/#{params[:id]}"
   end
@@ -15,8 +32,10 @@ class UsersController < ApplicationController
 
   def update
     @user = api_post "users/#{params[:id]}", params
-    render :show unless @user.nil?
+    redirect_to :show, id: params[:id] unless @user.nil?
   end
+
+
 
   def destroy
     # TODO: Send delete request to API
